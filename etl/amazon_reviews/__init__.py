ca2cec0212c30bc48b5f26307d758a5ccb93f5a6
JangasCodingplace <janisgoesser92@gmail.com>
2021-06-12 01:34:15 +0200

    Add file rules

--- etl/amazon_reviews/__init__.py
+++ etl/amazon_reviews/__init__.py
@@ -1,7 +1,15 @@
+import logging
+import os
+import pandas as pd
 from utils import BaseETL
 
 
+logger = logging.getLogger(__name__)
+
+
 class ETL(BaseETL):
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+
     def _load(cls, is_dummy: bool):
         pass
 
--- etl/utils/etl.py
+++ etl/utils/etl.py
@@ -1,11 +1,29 @@
-from abc import ABC, abstractclassmethod
+import os
+from abc import ABC, abstractclassmethod, abstractproperty
 
 
 class BaseETL(ABC):
     def __init__(self, is_dummy: bool = False, *args, **kwargs):
+        self.data_dir = os.path.join(self.file_dir, "data")
+        assert os.path.isdir(self.data_dir), (
+            'A directory named `data` must exist.'
+        )
+        self.sample_data_dir = os.path.join(self.file_dir, "sample_data")
+        assert os.path.isdir(self.sample_data_dir), (
+            'A directory named `sample_data_dir` must exist.'
+        )
+
+        self.output_dir = os.path.join(self.file_dir, "output")
+        if not os.path.isdir(self.output_dir):
+            os.mkdir(self.output_dir)
+
         self._load(is_dummy=is_dummy)
         self._transform()
         self._extract()
+
+    @abstractproperty
+    def file_dir(self):
+        pass
 
     @abstractclassmethod
     def _load(cls, is_dummy: bool):
