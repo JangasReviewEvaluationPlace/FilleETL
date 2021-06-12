068c2730c7442ce2ca2ec17c318cd6e9cfc5b5c9
JangasCodingplace <janisgoesser92@gmail.com>
2021-06-12 02:09:27 +0200

    Refactore extract method

--- etl/amazon_reviews/__init__.py
+++ etl/amazon_reviews/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import pandas as pd
+from typing import List
 from utils import BaseETL, GenericETLLoggingDecorators
 
 
@@ -10,29 +11,33 @@ logger = logging.getLogger(__name__)
 class ETL(BaseETL):
     file_dir = os.path.dirname(os.path.realpath(__file__))
 
-    @GenericETLLoggingDecorators.extract(data_variable_name="dataframes")
-    def _extract(self, is_dummy: bool):
+    def __get_csv_files(self, is_dummy: bool) -> List[str]:
         df_dir = self.file_dir if not is_dummy else self.sample_data_dir
-        csv_files = [
-            csv_file for csv_file in os.listdir(df_dir)
+        return [
+            os.path.join(df_dir, csv_file)
+            for csv_file in os.listdir(df_dir)
             if csv_file.endswith(".csv")
         ]
-        already_processed_files = [
+
+    def __already_processed_files(self) -> List[str]:
+        return [
             csv_file for csv_file in os.listdir(self.output_dir)
             if csv_file.endswith(".csv")
         ]
 
+    @GenericETLLoggingDecorators.extract(data_variable_name="dataframes")
+    def _extract(self, is_dummy: bool):
+        csv_files = self.__get_csv_files(is_dummy=is_dummy)
+        already_processed_files = self.__already_processed_files()
+
         self.dataframes = []
         column_names = ('rating', 'header', 'body')
         for csv_file in csv_files:
-            if csv_file in already_processed_files:
+            csv_file_name = os.path.basename(csv_file)
+            if csv_file_name in already_processed_files:
                 continue
             self.dataframes.append(
-                pd.read_csv(
-                    os.path.join(df_dir, csv_file),
-                    names=column_names,
-                    header=None
-                )
+                pd.read_csv(csv_file, names=column_names, header=None)
             )
 
     def _transform(self):
